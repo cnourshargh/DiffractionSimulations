625a82ab8292dac218fa7bf9bba6c3adf60c7319
cnourshargh <cami.n@hotmail.co.uk>
2022-06-21 12:25:08 +0100

    added separate rotations for concentric rings to SPP phase profile code

--- Profile_library/SPP.m
+++ Profile_library/SPP.m
@@ -3,25 +3,38 @@ x = linspace(-n/2,n/2,n);
 y = x;
 [X,Y] = meshgrid(x,y);
 M = zeros(n);
-p = 3;
+p = 2;
 for i = 1:n
     for j = 1:n
        M(i,j) = p*atan2(X(i,j),Y(i,j));
     end
 end
-M = PhaseProfile2IncidentBeam(M);
 
+B = (X.^2+Y.^2>=2^9); 
+M(B) = M(B)+pi*p*24;
+
+B = (X.^2+Y.^2>=2^7); 
+M(B) = M(B)+pi*p*24;
+
+M = PhaseProfile2IncidentBeam(M);
+% M2 = PhaseProfile2IncidentBeam(M2);
 theta = atan2(X,Y);
 
+% A =  theta=pi2; 
+% M(A) = exp(1ipi);
+% 
+% A =  theta=pi6; 
+% M(A) = exp(1i0);
 
-A =  abs(theta)<=2*pi/3; 
-M(A) = exp(1i*pi);
+% B = (X.^2+Y.^2=17^2) & (X.^2+Y.^2=20^2); 
+% M(B) = exp(1ippi);
 
-% B = (X.^2+Y.^2>=17^2) & (X.^2+Y.^2<=20^2); 
-% M(B) = exp(1i*p*pi);
+% B = (X.^2+Y.^2=20^2);
+% M(B) = exp(pip1i);
 
-% B = (X.^2+Y.^2>=20^2);
-% M(B) = 1;
+B = (X.^2+Y.^2>=2^11); 
+M(B) = 1;
 
-B = (X.^2+Y.^2>=22.5^2); 
+B = (X.^2+Y.^2>=2^13); 
 M(B) = 0;
+%M2(B) = 0;
